0ad172f38f4a7b4560477c5c62ca843cc58d0e2d
binbin.xiong <binbin.xiong@cloudchef.io>
2020-05-06 17:39:34 +0800

    Revert "FIx Bug: 入会审核【驳回】操作无效"
    This reverts commit a257c79e6595276b02bfd458c5b1989effd3435d.

--- src/components/realNameAuthentication/index.jsx
+++ src/components/realNameAuthentication/index.jsx
@@ -35,8 +35,8 @@ class RealNameAuthentication extends Component {
             this.getList();
         }
     }
-    getList(params){
-        params = params || {
+    getList(){
+        let params = {
             'auditStatus':1,
             'orderField': '',
             'orderType': '',
@@ -45,12 +45,14 @@ class RealNameAuthentication extends Component {
         };
         getAlumniAuditList(params).then(res=>{
             if(res.success){
-                res.data = res.data || {};
-                this.setState({
-                    dataList:this.buildData(res.data.items || []),
-                    total:res.data.totalCount || 0,
-                    loading:false
-                });
+                if(res.data){
+                    this.setState({
+                        dataList:this.buildData(res.data.items),
+                        total:res.data.totalCount,
+                        loading:false
+                    });
+                }
+
             }
         });
     }
@@ -84,10 +86,10 @@ class RealNameAuthentication extends Component {
             loading:true
         });
     }
-    
-    searchs(){
+    async searchs(){
         const {param}=this.state;
-        this.getList({...param});
+        // param.id=Number(param.id);
+        const res =getAlumniAuditList({...param});
     }
     IdChange(e,key){
         const { param } = this.state;
@@ -121,29 +123,8 @@ class RealNameAuthentication extends Component {
             }
         });
     }
-    onNoClick(record){
-        let {key,id,username='',alumniModelName=''} = record;
-        let _this = this;
-        confirm({
-            title: '您确认驳回该请求?',
-            content: '请您核实用户('+username+')基本信息，确认驳回其加入组织('+alumniModelName+')！',
-            onOk() {
-                AlumniAuditRefuse(id).then(res=>{
-                    if(res.success){
-                        const dataList = [..._this.state.dataList];
-                        _this.setState({ dataList: dataList.filter(item => item.id !== id) });
-                        message.success('驳回通过！');
-                    }else{
-                        message.error('审批失败！');
-                    }
-                });
-
-            },
-            onCancel() {
-                console.log('取消');
-
-            }
-        });
+    onNoClick(id){
+        console.log(id);
     }
     render() {
         let columns = [
